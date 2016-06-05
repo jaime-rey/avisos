5d6511f698cb1c6e0468205b071c3d1cae04f08b
jaimereycasado <jaimereycasado@gmail.com>
2016-06-05 23:26:11 +0200

    Añadidos item Nuevo y Salida

--- app/src/main/java/com/fullmind/avisos/AvisosActivity.java
+++ app/src/main/java/com/fullmind/avisos/AvisosActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,14 +24,6 @@ public class AvisosActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         mListView = (ListView) findViewById(R.id.avisos_list_view);
 
@@ -47,6 +40,16 @@ public class AvisosActivity extends AppCompatActivity {
         );
 
         mListView.setAdapter(arrayAdapter);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
@@ -64,7 +67,13 @@ public class AvisosActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_nuevo) {
+            Log.d( getLocalClassName(), "Crear nuevo aviso" );
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_salir) {
+            finish();
             return true;
         }
 
