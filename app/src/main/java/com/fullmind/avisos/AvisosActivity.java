d860ee8717cf32ce435898fa1f6b2fb0fd470f32
jaimereycasado <jaimereycasado@gmail.com>
2016-06-28 19:25:15 +0200

    Final con bugs en algunos tios, (aviso importante)

--- app/src/main/java/com/fullmind/avisos/AvisosActivity.java
+++ app/src/main/java/com/fullmind/avisos/AvisosActivity.java
@@ -11,16 +11,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.ActionMode;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
@@ -54,12 +59,7 @@ public class AvisosActivity extends AppCompatActivity {
         mDbAdapter = new AvisosDBAdapter(this);
         mDbAdapter.open();
 
-        if (savedInstanceState == null) {
-            mDbAdapter.deleteAllReminders();
 
-            mDbAdapter.createReminder("Primer reminder, aviso creado", true);
-            mDbAdapter.createReminder("Segundo reminder, aviso nuevo", false);
-        }
 
         Cursor cursor = mDbAdapter.fetchAllReminders();
 
@@ -121,11 +121,12 @@ public class AvisosActivity extends AppCompatActivity {
                     public void onItemClick ( AdapterView< ? > parent, View view, int position, long id ) {
 
                         if ( position == 0 ) {
-                            Toast.makeText( AvisosActivity.this, "editar "  + masterListPosition,
-                                    Toast.LENGTH_LONG ).show();
+                            int nId = getIdFromPosition( masterListPosition );
+                            Aviso aviso = mDbAdapter.fetchReminderById( nId );
+                            fireCustomDialog( aviso );
                         } else {
-                            Toast.makeText(AvisosActivity.this, "borrar "
-                                    + masterListPosition, Toast.LENGTH_SHORT).show();
+                            mDbAdapter.deleteReminderById( getIdFromPosition( masterListPosition ) );
+                            mCursorAdapter.changeCursor( mDbAdapter.fetchAllReminders( ) );
                         }
                         dialog.dismiss();
                     }
@@ -194,25 +195,69 @@ public class AvisosActivity extends AppCompatActivity {
         return true;
     }
 
+    private void fireCustomDialog ( final Aviso aviso ) {
+        //custom dialog
+        final Dialog dialog = new Dialog( this );
+        dialog.requestWindowFeature( Window.FEATURE_NO_TITLE );
+        dialog.setContentView( R.layout.dialog_custom );
+
+        TextView titleView = ( TextView ) dialog.findViewById( R.id.custom_title);
+        final EditText editCustom = ( EditText ) dialog.findViewById( R.id.custom_edit_reminder );
+        Button commitButton = ( Button ) dialog.findViewById( R.id.custom_button_commit );
+        final CheckBox checkBox = ( CheckBox ) dialog.findViewById( R.id.custom_check_box );
+        LinearLayout rootLayout = ( LinearLayout ) dialog.findViewById( R.id.custom_root_layout );
+        final boolean isEditOperation = ( aviso != null );
+
+        //esto es para un edit
+        if ( isEditOperation ) {
+            titleView.setText( "Editar aviso" );
+            checkBox.setChecked( aviso.getImportant() == 1 );
+            editCustom.setText( aviso.getContent() );
+        }
+
+        commitButton.setOnClickListener( new View.OnClickListener( ){
+
+            @Override
+            public void onClick(View v) {
+                String reminderText = editCustom.getText().toString();
+                if ( isEditOperation ){
+                    Aviso reminderEdited = new Aviso( aviso.getId(), checkBox.isChecked() ? 1 : 0,
+                            reminderText );
+                    mDbAdapter.updateReminder( reminderEdited );
+
+                } else {
+                    mDbAdapter.createReminder( reminderText, checkBox.isChecked() );
+                }
+                mCursorAdapter.changeCursor( mDbAdapter.fetchAllReminders() );
+                dialog.dismiss();
+            }
+        });
+
+        Button buttonCancel = ( Button ) dialog.findViewById( R.id.custom_button_cancel );
+        buttonCancel.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v){
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_nuevo) {
-            Log.d(getLocalClassName(), "Crear nuevo aviso");
+        switch ( item.getItemId() ){
+            case R.id.action_nuevo:
+                fireCustomDialog(null);
+                return true;
+            case R.id.action_salir:
+                finish();
+                return true;
+            default:
+                return false;
         }
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_salir) {
-            finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
